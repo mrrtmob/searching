89e57bae6a1220f4aa6c5de3041a5f87c6c965f9
Tmob <mrrtmob@gmail.com>
2024-02-19 13:23:06 +0700

    connect to seeddms database

--- src/routes/seeddms.js
+++ src/routes/seeddms.js
@@ -16,5 +16,21 @@ const seeddms = (app) => {
             })
         }
     })
+    app.get('/api/document/:id', async (req, res) => {
+        const { id } = req.params
+        try {
+            const response = await Document.findOne({ where: { id } })
+            res.status(200).json({
+                code: 200,
+                data: response
+            })
+        }
+        catch (e) {
+            res.status(400).json({
+                code: 400,
+                message: e
+            })
+        }
+    })
 }
 module.exports = { seeddms }
